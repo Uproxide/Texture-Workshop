74ecca64f1dc71c9769a76adeb511f44e78d08dd
M336 <75208577+M336G@users.noreply.github.com>
2024-09-14 21:34:40 +0200

    Fix missing cert verification

--- src/TexturePackInfo.hpp
+++ src/TexturePackInfo.hpp
@@ -195,7 +195,8 @@ public:
 
             auto req = web::WebRequest();
             req.userAgent(fmt::format("TextureWorkshopMod/{}", Mod::get()->getVersion()));
-
+            req.certVerification(Mod::get()->getSettingValue<bool>("cert-verification"));
+            
             m_downloadTP.setFilter(req.get(texturePack->download));
     }
 
